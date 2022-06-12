18207bdcf363f3608968cf2741cc2b1727619915
Gustavo Pessoa <gupessoa@live.com>
2022-06-12 18:21:14 -0300

    criando migration model e relações de post

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function users()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
