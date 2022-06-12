d1755f29bb73f8c71d7dbeb622653f86d240d2d9
Gustavo Pessoa <gupessoa@live.com>
2022-06-12 13:07:33 -0300

    Modificando redirecionamento de Login e criando a regra de autenticação para mostrar perfil do usuário

--- resources/views/blog/master.blade.php
+++ resources/views/blog/master.blade.php
@@ -54,11 +54,21 @@
                 </ul>
             @endguest
             @auth
-                <ul>
-                    <li class="dropDown">
-                        <a href="#" class="lgAdmin">Usuário Nome</a>
-                    </li>
-                </ul>
+{{--                <ul>--}}
+{{--                    <li class="dropDown">--}}
+{{--                        <a href="#" class="lgAdmin">{{ auth()->user()->name }}</a>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+                <div class="dropdown">
+                    <button class="dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
             @endauth
         </div>
     </nav>
